4b9b72a7601336dc41032c606559f5ee2c5a2c47
Vsavilov <vsavilov@student.42madrid.com>
2022-01-14 16:01:29 +0100

    lstnewchar

--- src/ft_lstnewchar.c
+++ src/ft_lstnewchar.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnewchar.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Vsavilov <Vsavilov@student.42Madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 15:56:48 by Vsavilov          #+#    #+#             */
+/*   Updated: 2022/01/14 16:01:04 by Vsavilov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 
-t_listc *ft_lstnewchar(char *str)
+int	*ft_lstnewchar(char *str)
 {
-	t_listc *listc;
+	t_listc	*listc;
 
 	listc = (t_listc *)ft_calloc(1, sizeof(*listc));
 	if (!listc)
